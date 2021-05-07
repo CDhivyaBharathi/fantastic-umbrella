76dd41f2fb77b033a91d33cb789b1ca4602b17b0
C Dhivya Bharathi <67786951+CDhivyaBharathi@users.noreply.github.com>
2021-05-07 17:11:58 +0530

    Update Pro101.py

--- Pro101/Pro101.py
+++ Pro101/Pro101.py
@@ -12,9 +12,9 @@ class TransferData:
         for root , dirs, files in os.walk(file_from):
 
             for filename in files:
-                local_path = os.path.jooin(root , filename)
+                local_path = os.path.join(root , filename)
                 relative_path = os.path.relpath(local_path , file_from)
-                dropbox_path = os.path.join(file_tp , relative_path)
+                dropbox_path = os.path.join(file_to , relative_path)
 
                 with open(local_path , 'rb') as f:
                     dbx.files_upload(f.read() , dropbox_path , mode=WriteMode('overwrite'))
